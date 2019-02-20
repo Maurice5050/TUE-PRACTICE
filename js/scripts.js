d284a83b540f2175aa5293be1c9424c7e155b207
mkoyandhe@gmail.com <mkoyandhe@gmail.com>
2019-02-20 17:24:27 +0300

    modified files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,58 @@
-$("ul#user").children("li").first().click(function() {
-        $(this).remove();
+//
+// $(document).ready(function() {
+//        $("button#hello").click(function() {
+//          $("ul").prepend("<li>Hello!</li>");
+//        });
+//
+//        $("button#goodbye").click(function() {
+//          $("ul").prepend("<li>Goodbye!</li>");
+//        });
+//
+//        $("button#stop").click(function() {
+//          $("ul").prepend("<li>Stop copying me!</li>");
+/ $(document).ready(function() {
+
+        $("div#click-one").click(function(event) {
+          var whatToSay = "Hello!";
+          alert(whatToSay);
+        });
+
+        $("div#click-two").click(function(event) {
+          alert(whatToSay);
+        });
+      });/
+     });
+//      });
+$(document).ready(function() {
+        $("button#hello").click(function() {
+          $("ul#user").prepend("<li>Hello!</li>");
+          $("ul#webpage").prepend("<li>Why hello there!</li>");
+           $('li').css('background-color', 'green');
+           $('li').click(function() {
+          alert('hi');
+          $("ul#user").children("li").first().click(function() {
+        alert('hi');
       });
       $("ul#webpage").children("li").first().click(function() {
-        $(this).remove();
+        alert('hi');
       });
-    
+        });
+        });
+
+        $("button#goodbye").click(function() {
+          $("ul#user").prepend("<li>Goodbye!</li>");
+          $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+          $('li').css('background-color', 'blue');
+        });
+        $("ul#user").children("li").first().click(function() {
+        alert('hi');
+      });
+      $("ul#webpage").children("li").first().click(function() {
+        alert('hi');
+      });
+
+        $("button#stop").click(function() {
+          $("ul#user").prepend("<li>Stop copying me!</li>");
+          $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+        });
+      });
