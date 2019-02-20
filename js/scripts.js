4c5d220bd8cc1f3fb8dc473d7eb3739e915d137c
mkoyandhe@gmail.com <mkoyandhe@gmail.com>
2019-02-20 14:17:28 +0300

    added files

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,7 @@
-alert("working");
-
-jQuery('h1').click(function(){
-alert("This is a heading tag")
-});
-jQuery('p').click(function(){
-    alert("This is a paragraph")
-  })
-  jQuery('img').click(function(){
-    alert("This is an image")
-  });
-  Query("h1").click(function() {
-    alert("This is not a header.");
-    alert("I told you, THIS IS A HEADER!");
-  });
-  $(document).ready(function() {
-    $("p").click(function() {
-      $("img").show();
-    });
-  });
-  $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
-  });
-});
+$("ul#user").children("li").first().click(function() {
+        $(this).remove();
+      });
+      $("ul#webpage").children("li").first().click(function() {
+        $(this).remove();
+      });
+    
